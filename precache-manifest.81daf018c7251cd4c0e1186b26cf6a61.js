b63ea459686da8877d9e97fa04f06b0cdf800fd8
Russ <851911336@qq.com>
2020-08-15 09:43:40 +0800

    deploy

---
+++ precache-manifest.81daf018c7251cd4c0e1186b26cf6a61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3605e2dee1ceb2835325",
+    "url": "/tally-book-websit/css/app.55433e1c.css"
+  },
+  {
+    "revision": "ba78b11f861eb376901f78596faefcbc",
+    "url": "/tally-book-websit/index.html"
+  },
+  {
+    "revision": "3605e2dee1ceb2835325",
+    "url": "/tally-book-websit/js/app.f61568e2.js"
+  },
+  {
+    "revision": "4cb358d45cd0d4296ad4",
+    "url": "/tally-book-websit/js/chunk-vendors.91f4e939.js"
+  },
+  {
+    "revision": "a7a2c52c0b639f56f7eb6e7c6990217c",
+    "url": "/tally-book-websit/manifest.json"
+  },
+  {
+    "revision": "b5233e56ef50c120b047d3e9e5946316",
+    "url": "/tally-book-websit/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tally-book-websit/robots.txt"
+  }
+]);
